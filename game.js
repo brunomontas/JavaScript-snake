c11c9445489e035375c2f9bb704e9a07f3eac452
Bruno Monteiro Silva <brunomonteirosilva@MacBook-de-Bruno.local>
2019-01-28 21:45:08 +0000

    added food and movement

--- game.js
+++ game.js
@@ -3,9 +3,15 @@
 /** CONSTANTS **/
 const CANVAS_BACKGROUND_COLOUR = 'black'
 const SNAKE_COLOUR = 'lightgreen';
+const FOOD_COLOUR = 'red';
 const gameScreen = document.getElementById('game');
+document.addEventListener("keydown", changeDirection)
 
-    
+// Horizontal velocity
+let dx = 10;
+// Vertical velocity
+let dy = 0;
+
 // Return a two dimensional drawing context
 let ctx = gameScreen.getContext("2d");
 //  Select the colour to fill the canvas
@@ -20,9 +26,17 @@ var State = class State {
     }
 
     initialize() {
-        this.snake = new Snake(Math.floor((gameScreen.width - 10)*Math.random()), Math.floor((gameScreen.height - 10)*Math.random()));
+        this.snake = new Snake(Math.floor(Math.random()*30)*10,Math.floor(Math.random()*30)*10);
+        this.food = new Food(Math.floor(Math.random()*30)*10, Math.floor(Math.random()*30)*10);
     }
 
+    detectColision () {
+
+        if(this.snake.x === this.food.x && this.snake.y === this.food.y){
+            this.food.movePosition();
+            console.log("eat foot");
+        }
+    }
     
 }
 
@@ -31,8 +45,9 @@ var Snake = class Snake {
         this.x = x;
         this.y = y;
         this.draw();
-        
+
     }
+
     draw() {
         ctx.fillStyle = SNAKE_COLOUR;
         // Draw a "filled" rectangle to represent the snake part at the coordinates
@@ -41,27 +56,80 @@ var Snake = class Snake {
 
     }
 
+    moveSnake() {
+
+            ctx.fillStyle = SNAKE_COLOUR;
+            this.x = this.x + dx;
+            this.y = this.y + dy;
+            // Draw a "filled" rectangle to represent the snake part at the coordinates
+            // the part is located
+            ctx.fillRect(this.x, this.y, 10, 10);  
+    }
 }
 
+var Food = class Food {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.draw();
+
+    }
+
+    draw() {
+        ctx.fillStyle = FOOD_COLOUR;
+        // Draw a "filled" rectangle to represent the food at the coordinates
+        // the part is located
+        ctx.fillRect(this.x, this.y, 10, 10);
+
+    }
+
+    movePosition(){
+        this.x = Math.floor(Math.random()*30)*10;
+        this.y = Math.floor(Math.random()*30)*10;
+    }
+}
+
+function changeDirection(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+      }
+      if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+      }
+      if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+      }
+      if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+      }
+    }
+
 function start() {
-    setInterval(function step() {
+    let state = new State();
+    setInterval(() => {
+        state.detectColision();
         ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
         ctx.fillRect(0, 0, gameScreen.width, gameScreen.height);
-        let state = new State();
-      }, 100);
+        state.snake.moveSnake();
+        state.food.draw();
+        
+
+    }, 100);
 
 }
 
 start();
-
-
-
-
-
-
-
-    
-
-
-
-
